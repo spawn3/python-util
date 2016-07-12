7449deb9c8f5505274033db840061140c343c37e
spawn3 <dongguanjun@adesk.com>
2016-07-12 12:57:57 +0800

    cmake tutorial

---
+++ cmake/tutorial/example1/include/example1.h
@@ -0,0 +1,7 @@
+#ifndef __EXAMPLE1_H
+#define __EXAMPLE1_H
+
+extern int add(int x, int y);
+
+
+#endif
---
+++ cmake/tutorial/example2/include/example2.h
@@ -0,0 +1,7 @@
+#ifndef __EXAMPLE2_H
+#define __EXAMPLE2_H
+
+extern int multi(int x, int y);
+
+
+#endif
