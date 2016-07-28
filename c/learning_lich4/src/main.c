66d2f16aacb0843c0f294157761214a51527fead
bianwenhui <bianwenhui@huayunwangji.com>
2016-07-28 10:24:43 +0800

    learning lich4 added

---
+++ c/learning_lich4/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "config.h"
+
+#define DBG_SUBSYS S_LIBINTERFACE
+
+#include "configure.h"
+#include "dbg.h"
+
+// #include "hello.h"
+
+int main() {
+    printf("Hello, world\n");
+    return 0;
+}
