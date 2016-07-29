cb8812041183ac8a131c88b7bb1048bc271f53b7
bianwenhui <bianwenhui@huayunwangji.com>
2016-07-29 19:02:08 +0800

    buffer_t depends env_init_simple

--- c/learning_lich4/src/main.c
+++ c/learning_lich4/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/param.h>
 
 #include "config.h"
 
@@ -9,12 +10,36 @@
 #include "lichbd.h"
 #include "utils.h"
 
-// #include "hello.h"
+#include "buffer.h"
 
-int main() {
+static inline void chunk_bound(uint64_t fsize, uint32_t idx, uint64_t *offset, int *size) {
+        *offset = idx * LICH_CHUNK_SPLIT;
+        *size = MIN(MAX(fsize - *offset, 0), LICH_CHUNK_SPLIT);
+}
+
+void test_page_bound() {
+        uint64_t offset;
+        int size;
+
+        chunk_bound(5, 0, &offset, &size);
+
+        printf("offset = %lu, size = %d\n", offset, size);
+}
+
+void test_buffer() {
+        buffer_t buf;
+
+        mbuffer_init(&buf, 0);
+
+        mbuffer_appendmem(&buf, "abc", 3);
+
+        printf("first: %s\n", (char *)mbuffer_head(&buf));
+
+        mbuffer_free(&buf);
+}
+
+void test_lichbd() {
         int ret;
-        printf("Hello, world\n");
-
         ret = lichbd_init("");
         if (ret) {
                 GOTO(err_ret, ret);
@@ -24,6 +49,26 @@ int main() {
         if (ret) {
                 GOTO(err_ret, ret);
         }
+err_ret:
+        (void)0;
+}
+
+
+int main() {
+        int ret;
+
+        printf("Hello, world\n");
+        test_page_bound();
+
+        // mem_cache_init();
+        // mem_hugepage_init();
+        ret = env_init_simple("test");
+        if (ret) {
+                goto err_ret;
+        }
+
+        printf("-------------------------------------\n");
+        test_buffer();
 
         return 0;
 err_ret:
