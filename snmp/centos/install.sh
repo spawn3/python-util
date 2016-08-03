c76897640be7b9fabe1a15b1bd9536a07eb9ceae
gj <dongguanjun@fusionstack.cn>
2016-08-03 18:13:51 +0800

    snmp

---
+++ snmp/centos/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+yum install -y net-snmp net-snmp-utils snmptt 
+yum install -y mariadb-server mariadb
+yum install -y zabbix22-server-mysql zabbix22 zabbix22-web-mysql zabbix22-agent
+
+cp mibs/LICH-MIB.txt /usr/share/snmp/mibs/
+
+
