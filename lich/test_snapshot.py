f4fd0886cdf53491b62ae610ba48c3ac61e12a7d
gj <dongguanjun@fusionstack.cn>
2016-11-01 10:47:37 +0800

    test lich

--- lich/test_snapshot.py
+++ lich/test_snapshot.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+from lich.umptypes import UmpPath
 from base import TestBase
 
 
@@ -11,29 +12,29 @@ class TestAll(TestBase):
     def setUp(self):
         super(TestAll, self).setUp()
 
-        self.pname_01 = 'snap.a'
-        self.vname_01 = 'snap.a/b'
-        self.sname_01 = 'snap.a/b@c'
-        self.clone_01 = 'snap.a/d'
+        self.pname_01 = UmpPath('snap.a')
+        self.vname_01 = UmpPath('snap.a/b')
+        self.sname_01 = UmpPath('snap.a/b@c')
+        self.clone_01 = UmpPath('snap.a/d')
 
         self.size = 1024*1024*1024
         self.new_size = 2 * self.size
 
         # clean
-        self._del_volume(self.clone_01)
-        self._del_snapshot(self.sname_01)
-        self._del_volume(self.vname_01)
-        self._del_pool(self.pname_01)
+        self.del_volume(self.clone_01)
+        self.del_snapshot(self.sname_01)
+        self.del_volume(self.vname_01)
+        self.del_pool(self.pname_01)
 
-        self._create_pool(self.pname_01)
-        self._create_volume(self.vname_01, self.size)
-        self._create_snapshot(self.sname_01)
+        self.create_pool(self.pname_01)
+        self.create_volume(self.vname_01, self.size)
+        self.create_snapshot(self.sname_01)
 
     def tearDown(self):
-        self._del_volume(self.clone_01)
-        self._del_snapshot(self.sname_01)
-        self._del_volume(self.vname_01)
-        self._del_pool(self.pname_01)
+        self.del_volume(self.clone_01)
+        self.del_snapshot(self.sname_01)
+        self.del_volume(self.vname_01)
+        self.del_pool(self.pname_01)
 
     def test_create(self):
         pass
