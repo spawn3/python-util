8886c9487003636a38e09340e918f4e19cc75f04
gj <dongguanjun@fusionstack.cn>
2016-11-02 14:51:02 +0800

    install zabbix & snmp

--- snmp/centos/install.sh
+++ snmp/centos/install.sh
@@ -1,7 +1,39 @@
 #!/usr/bin/env bash
 
-yum install -y net-snmp net-snmp-utils snmptt 
+# PDCA
+# centos 7
+
+# install and init zabbix
+#
+# https://www.zabbix.com/documentation/2.2/manual/installation/install_from_packages
+# https://www.zabbix.com/documentation/2.4/manual/config/items/itemtypes/snmptrap
+
+# NOTE:
+# zabbix host interface ip === 192.168.151.33
+
+
+# LAMP
+# php -i
+yum install -y httpd
 yum install -y mariadb-server mariadb
-yum install -y zabbix22-server-mysql zabbix22 zabbix22-web-mysql zabbix22-agent
+
+yum install -y zabbix22-server-mysql zabbix22 zabbix22-web-mysql
+
+yum install -y zabbix22-agent
+
+# SNMP
+yum install -y net-snmp net-snmp-utils snmptt 
 
 cp mibs/LICH-MIB.txt /usr/share/snmp/mibs/
+
+
+# start services
+
+# snmptrapd -d -On -Lo
+# systemctl start snmptt
+# systemctl start httpd
+# systemctl start zabbix-server
+
+
+# CHECK
+# snmptrap -v 2c -c public 192.168.1.69:162 "" LICH-MIB::trapEvent SNMPv2-MIB::sysLocation.0 s "just here"
