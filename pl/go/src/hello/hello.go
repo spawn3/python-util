389580404c77a8473ea9ed156d8ae950061ab626
gj <dongguanjun@fusionstack.cn>
2019-01-17 23:19:16 +0800

    sanxue

--- pl/go/src/hello/hello.go
+++ pl/go/src/hello/hello.go
@@ -5,6 +5,7 @@ import (
     "util"
     "wbuf"
     "simplemath"
+    "time"
 )
 
 func test1() {
@@ -60,8 +61,42 @@ func testMap() {
     }
 }
 
+func say(s string) {
+    for i := 0; i < 5; i++ {
+        time.Sleep(100 * time.Millisecond)
+        fmt.Println(s)
+    }
+}
+
+func sum(s []int, c chan int) {
+    sum := 0
+    for _, v := range s {
+        sum += v
+    }
+
+    c <- sum
+}
+
+func testChan() {
+    s := []int{7, 2, 8, -9, 4, 0}
+
+    c := make(chan int)
+
+    go sum(s[:len(s)/2], c)
+    go sum(s[len(s)/2:], c)
+
+    x, y := <-c, <- c
+
+    fmt.Println(x, y, x+y)
+}
+
 func main() {
-    testSlice()
-    testMap()
-    testOOP()
+    // testSlice()
+    // testMap()
+    // testOOP()
+
+    // go say("World")
+    // say("Hello")
+
+    testChan()
 }
